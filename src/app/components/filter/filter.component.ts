027dca4c25d2a1ebf438f13dc6ad618ac5210d8c
Tom <tom@cadcom.fr>
2025-06-04 02:00:01 +0200

    feat: create filters selection layout

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShipmentStatus } from 'src/app/enums/shipment-status.enum';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   standalone: true,
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css'],
-  imports: [],
+  imports: [CommonModule, MatChipsModule],
 })
-export class FilterComponent {}
+export class FilterComponent {
+  public status: ShipmentStatus[] = Object.values(ShipmentStatus);
+
+  constructor() {}
+}
